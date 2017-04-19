f9dbc32ef833707726b1d6e2ea0b5fae0745ef28
Son Nguyen <ntson2002@gmail.com>
2017-04-19 18:11:26 +0900

    Upload

--- lstm-tagger-v2-dev/exercises/test.py
+++
@@ -1,6 +0,0 @@
-from glob import glob
-from pprint import pprint
-
-parent = "/home/s1520203/Bitbucket/lstm-crf-tagging/experiments/vn-ner/scripts-3/models/"
-paths = glob(parent+"*/")
-pprint (paths)
--- lstm-tagger-v2-dev/scripts/ner/train.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-FOLDER=/Users/$USER/Bitbucket/lstm-crf-tagging/lstm-tagger-v2/data/ner
-PROGRAM=/Users/sonnguyen/Bitbucket/lstm-crf-tagging/lstm-tagger-v2
-EVALTOOL=/Users/sonnguyen/Bitbucket/lstm-crf-tagging/lstm-tagger-v2/evaluation
-
-
-#ln -s $EVALTOOL evaluation
-python $PROGRAM/train.py --train $FOLDER/train.conll  --dev $FOLDER/val.conll --test $FOLDER/test.conll
